895186c6e3e099b2993b4eaafa14489f41ab4bfd
mochammadilham06 <stormsykres1006@gmail.com>
2022-11-07 04:50:32 +0700

    task/React-Helemt: Complete Added React Helemt

--- portfolio-website/src/modules/Skills/index.js
+++ portfolio-website/src/modules/Skills/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { FooterWhite, Hero, NavComponent, SkillDetail } from "../../components";
+import { Helmet } from "react-helmet";
 
 const setActive = "skills";
 const SkillPage = () => {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Skills Page</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
       <NavComponent setActive={setActive} />
       <Hero title1={"LEARNED"} title2={"SKILL"} />
       <SkillDetail />
