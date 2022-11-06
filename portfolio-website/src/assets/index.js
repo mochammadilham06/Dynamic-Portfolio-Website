8e0c911a2ef2d33c1772d3dbe2daca1459ce9c01
mochammadilham06 <stormsykres1006@gmail.com>
2022-11-07 02:55:18 +0700

    task/FixResource:Fix Resource for Home Page

--- portfolio-website/src/assets/index.js
+++ portfolio-website/src/assets/index.js
@@ -1,7 +1,17 @@
-import { customSvg } from "./svgs";
-
 import PortfolioImage from "./images/portfolio.png";
 import SkillsImage from "./images/skills.png";
 import WelcomeImage from "./images/welcome.jpg";
+import ReactPicture from "./images/react.png";
+import TailwindPicture from "./images/tailwindcss.jpg";
+import JSPicture from "./images/js.png";
+import MyAvatar from "./images/myAvatar.png";
 
-export { PortfolioImage, SkillsImage, WelcomeImage };
+export {
+  PortfolioImage,
+  SkillsImage,
+  WelcomeImage,
+  ReactPicture,
+  TailwindPicture,
+  JSPicture,
+  MyAvatar,
+};
